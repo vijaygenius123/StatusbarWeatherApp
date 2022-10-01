fa0de52e7579afa90186c34361cc3fe7b5ad2c82
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-10-01 21:02:45 +0100

    Initial Commit

---
+++ StatusbarWeatherApp/StatusbarWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatusbarWeatherAppApp.swift
+//  StatusbarWeatherApp
+//
+//  Created by Vijayaraghavan Sundararaman on 01/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct StatusbarWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
